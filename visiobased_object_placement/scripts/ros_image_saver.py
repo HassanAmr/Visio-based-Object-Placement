f31bc7fb541eca2519b1ea49558f8560d8620295
Hassan <hassan.amr89@gmail.com>
2017-09-26 21:03:14 +0200

    added python image subscriber

---
+++ visiobased_object_placement/scripts/ros_image_saver.py
@@ -0,0 +1,56 @@
+#! /usr/bin/python
+# Copyright (c) 2015, Rethink Robotics, Inc.
+
+# Using this CvBridge Tutorial for converting
+# ROS images to OpenCV2 images
+# http://wiki.ros.org/cv_bridge/Tutorials/ConvertingBetweenROSImagesAndOpenCVImagesPython
+
+# Using this OpenCV2 tutorial for saving Images:
+# http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_gui/py_image_display/py_image_display.html
+
+# rospy for the subscriber
+import rospy
+# ROS Image message
+import message_filters
+from sensor_msgs.msg import Image
+# ROS Image message -> OpenCV2 image converter
+from cv_bridge import CvBridge, CvBridgeError
+import cv2 # OpenCV2 for saving an image
+
+
+# Instantiate CvBridge
+bridge = CvBridge()
+
+def callback(bg_image, scene_image):
+    # Solve all of perception here...
+    print("Received an image!")
+    try:
+        # Convert your ROS Image message to OpenCV2
+        cv2_bg_image = bridge.imgmsg_to_cv2(bg_image, "bgr8")
+        cv2_scene_image = bridge.imgmsg_to_cv2(scene_image, "bgr8")
+    except CvBridgeError, e:
+        print(e)
+    else:
+        #return [cv2_bg_image, cv2_scene_image]
+        cv2.imwrite('query_image.jpeg', cv2_bg_image)
+        cv2.imwrite('scene_image.jpeg', cv2_scene_image)
+        #rospy.signal_shutdown("Images acquired...")
+
+
+
+def main():
+    rospy.init_node('image_listener')
+    # Define your image topic
+    bg_image_topic = "/bg_subtracted_image"
+    scene_image_topic = "/kinect2/hd/image_color"
+
+    bg_image_sub = message_filters.Subscriber(bg_image_topic, Image)
+    scene_image_sub = message_filters.Subscriber(scene_image_topic, Image)
+    ts = message_filters.ApproximateTimeSynchronizer([bg_image_sub, scene_image_sub], 10, 1)
+    ts.registerCallback(callback)
+
+    # Spin until ctrl + c
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
